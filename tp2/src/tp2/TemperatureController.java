995abb8964ea41fd7eb9226cd2aa8d2f1e417fc4
timothe59 <timothe.ruc@gmail.com>
2016-11-25 14:02:52 +0100

    tp2

---
+++ tp2/src/tp2/TemperatureController.java
@@ -0,0 +1,49 @@
+package tp2;
+
+public class TemperatureController {
+	private TemperatureModel model ;
+	private TemperatureVue view = null ;
+	private ModifieTemperature modtemp = new ModifieTemperaturePlus1 ();
+	public TemperatureController ( TemperatureModel m ) {
+		model = m ;
+	}
+	public void augmenteDegresC (){
+		model . setC ( modtemp . augmenteDegres ( model . getC ()));
+		control ();
+	}
+	public void diminueDegresC (){
+		model . setC ( modtemp . diminueDegres ( model . getC ()));
+		control ();
+	}
+	public void fixeDegresC ( double tempC ){
+		model . setC ( tempC );
+		control ();
+	}
+	public void augmenteDegresF (){
+		model . setF ( modtemp . augmenteDegres ( model . getF ()));
+		control ();
+	}
+	public void diminueDegresF (){
+		model . setF ( modtemp . diminueDegres ( model . getF ()));
+		control ();
+	}
+	public void fixeDegresF ( double tempF ){
+		model . setF ( tempF );
+		control ();
+	}
+	public void control () {
+		if ( view != null ) {
+			if ( model . getC () > 40.0) {
+				view . enableWarningColor ();
+			} else {
+				view . disableWarningColor ();
+			}
+		}
+	}
+	public void addView ( TemperatureVue view ) {
+		this . view = view ;
+	}
+}
+
+
+
---
+++ tp2/src/tp2/TemperatureVueCelsuis.java
@@ -0,0 +1,35 @@
+package tp2;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Observable;
+
+public class TemperatureVueCelsuis extends TemperatureVue {
+	
+	public TemperatureVueCelsuis ( TemperatureModel modele ,TemperatureController controleur , int posX , int posY ) {
+		super ( " Temperature ␣ Celsuis " , modele , controleur , posX , posY );
+		setDisplay ( " " + model . getC ());
+		
+		addUpListener ( new ActionListener () {
+			public void actionPerformed ( ActionEvent e ) {
+				controller . augmenteDegresC ();
+			}});
+		addDownListener ( new ActionListener () {
+			public void actionPerformed ( ActionEvent e ) {
+				controller . diminueDegresC ();
+			}});
+		addDisplayListener ( new ActionListener () {
+			public void actionPerformed ( ActionEvent e ) {
+				double tempC = getDisplay ();
+				controller . fixeDegresC ( tempC );
+			}});
+	}
+	public void update ( Observable s , Object o ) {
+		setDisplay ( " " + model (). getC ());
+	}
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+}
